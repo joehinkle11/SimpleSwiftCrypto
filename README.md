65b61d4613a8dbd986e03d53de05c322fb0f2407
Joe Hinkle <joehinkle11@gmail.com>
2022-02-25 21:14:47 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Simple Swift Crypto
+
+I needed a simple way of doing both symmetric and asymmetric crypto without the headache. I also didn't need to store it in the Keychain. So I made this.
+
+Works on MacOS, iOS, WatchOS and TvOS.
+
+## Example Usage
+
+### AES Symmetric Encryption and Decryption
+
+```swift
+let testString: String = "Hello World!"
+let testData: Data = testString.data(using: .utf8)!
+
+let aesPrivateKey: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+let encryptedData: Data = aesPrivateKey.encrypt(data: testData)!
+
+let decryptedData: Data = aesPrivateKey.decrypt(data: encryptedData)!
+let decryptedString: String = String(data: decryptedData, encoding: .utf8)! // "Hello World!"
+```
+
+### RSA Asymmetric Encryption and Decryption
+
+```swift
+let testString: String = "Hello World!"
+let testData: Data = testString.data(using: .utf8)!
+
+let rsaPrivateKey: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+let rsaPublicKey: RSAPublicKey = rsaPrivateKey.extractPublicKey()
+let encryptedData: Data = rsaPublicKey.encrypt(data: testData)!
+
+let decryptedData: Data = rsaPrivateKey.decrypt(data: encryptedData)!
+let decryptedString: String = String(data: decryptedData, encoding: .utf8)! // "Hello World!"
+```
+
+### Exporting and Loading AES Secret Key
+
+```swift
+let aesPrivateKey: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+let aesPrivateKeyAsData: Data = aesPrivateKey.exportIvAndPrivateAES256Key()
+
+// send `aesPrivateKeyAsData` to another device securely, then...  
+let sameAesPrivateKeyFromData: AES256Key = .loadIvAndPrivateAES256Key(ivAndPrivateAES256Key: aesPrivateKeyAsData)!
+```
+
+### Exporting and Loading RSA Public Key
+
+```swift
+let rsaPrivateKey: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+let rsaPublicKey: RSAPublicKey = rsaPrivateKey.extractPublicKey()
+let rsaPublicKeyAsData: Data = rsaPublicKey.export()!
+
+
+// send `rsaPublicKeyAsData` to another device, then...  
+let sameRsaPublicKeyFromData: RSAPublicKey = .load(rsaPublicKeyData: rsaPublicKeyAsData)!
+```
+
+## Why did I make this?
+
+I'm working on an IDE for iPad/iOS called [App Maker Professional](https://www.appmakerios.com). A new feature I'm adding is multiplayer, and I don't want to have my users' files on the server. So I made this to be able to encrypt their files locally on device so the server can't read them.
+
+## Contributions
+
+If someone wants to add Keychain support, that'd be great. I'll take a look at a PR.
+
+## License
+
+[MIT](https://github.com/joehinkle11/SimpleSwiftCrypto/blob/main/LICENSE)
+
+## Sources
+
+- https://github.com/henrinormak/Heimdall
+
+- Apple Docs
+
+- Wikipedia
+
+- StackOverflow 😎
---
+++ Tests/SimpleSwiftCryptoTests/SimpleSwiftCryptoTests.swift
@@ -0,0 +1,123 @@
+import XCTest
+import SimpleSwiftCrypto
+
+final class SimpleSwiftCryptoTests: XCTestCase {
+    //
+    // AES
+    //
+    func testAESGenIsDifferentOnEachCall() {
+        let aesPrivateKey1: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        let aesPrivateKey2: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        
+        XCTAssertNotEqual(aesPrivateKey1.__debug_aes256Key, aesPrivateKey2.__debug_aes256Key)
+        XCTAssertNotEqual(aesPrivateKey1.__debug_iv, aesPrivateKey2.__debug_iv)
+        XCTAssertNotEqual(aesPrivateKey1.__debug_iv, aesPrivateKey1.__debug_aes256Key)
+        XCTAssertNotEqual(aesPrivateKey2.__debug_iv, aesPrivateKey2.__debug_aes256Key)
+    }
+    func testAESGenEncryptAndDecrypt() {
+        let testString: String = "Hello World!"
+        let testData: Data = testString.data(using: .utf8)!
+        
+        let aesPrivateKey: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        let encryptedData: Data = aesPrivateKey.encrypt(data: testData)!
+        let decryptedData: Data = aesPrivateKey.decrypt(data: encryptedData)!
+        
+        XCTAssertNotEqual(testData, encryptedData)
+        XCTAssertNotEqual(encryptedData, decryptedData)
+        XCTAssertEqual(testData, decryptedData)
+    }
+    func testAESGenEncryptWithDifferentKeysMakesDifferentMessage() {
+        let testString: String = "Hello World!"
+        let testData: Data = testString.data(using: .utf8)!
+        
+        let aesPrivateKey1: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        let aesPrivateKey2: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        let encryptedData1: Data = aesPrivateKey1.encrypt(data: testData)!
+        let encryptedData2: Data = aesPrivateKey2.encrypt(data: testData)!
+        let decryptedData1: Data = aesPrivateKey1.decrypt(data: encryptedData1)!
+        let decryptedData2: Data = aesPrivateKey2.decrypt(data: encryptedData2)!
+        
+        XCTAssertEqual(testData, testData)
+        XCTAssertNotEqual(testData, encryptedData1)
+        XCTAssertNotEqual(testData, encryptedData2)
+        XCTAssertNotEqual(encryptedData1, encryptedData2)
+        XCTAssertNotEqual(testData, encryptedData1)
+        XCTAssertNotEqual(testData, encryptedData2)
+        XCTAssertNotEqual(encryptedData1, decryptedData1)
+        XCTAssertNotEqual(encryptedData2, decryptedData2)
+        XCTAssertEqual(testData, decryptedData1)
+        XCTAssertEqual(testData, decryptedData2)
+    }
+    
+    func testAESExportAndLoads() {
+        let aesPrivateKey: AES256Key = SimpleSwiftCrypto.generateRandomAES256Key()!
+        let aesPrivateKeyAsData: Data = aesPrivateKey.exportIvAndPrivateAES256Key()
+        let sameAesPrivateKeyFromData: AES256Key = .loadIvAndPrivateAES256Key(ivAndPrivateAES256Key: aesPrivateKeyAsData)!
+        
+        XCTAssertEqual(aesPrivateKey.__debug_aes256Key, sameAesPrivateKeyFromData.__debug_aes256Key)
+        XCTAssertEqual(aesPrivateKey.__debug_iv, sameAesPrivateKeyFromData.__debug_iv)
+    }
+    
+    //
+    // RSA
+    //
+    func testRSAGen() {
+        let rsaPrivateKey1: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+        let rsaPrivateKey2: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+        
+        XCTAssertNotEqual(rsaPrivateKey1.__debug_privateKey, rsaPrivateKey2.__debug_privateKey)
+        XCTAssertNotEqual(rsaPrivateKey1.__debug_publicKey, rsaPrivateKey2.__debug_publicKey)
+        XCTAssertNotEqual(rsaPrivateKey1.__debug_publicKey, rsaPrivateKey1.__debug_privateKey)
+        XCTAssertNotEqual(rsaPrivateKey2.__debug_publicKey, rsaPrivateKey2.__debug_privateKey)
+    }
+    func testRSAGenGenEncryptAndDecrypt() {
+        let testString: String = "Hello World!"
+        let testData: Data = testString.data(using: .utf8)!
+        
+        let rsaPrivateKey: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+        let encryptedData: Data = rsaPrivateKey.extractPublicKey().encrypt(data: testData)!
+        let decryptedData: Data = rsaPrivateKey.decrypt(data: encryptedData)!
+        
+        XCTAssertNotEqual(testData, encryptedData)
+        XCTAssertNotEqual(encryptedData, decryptedData)
+        XCTAssertEqual(testData, decryptedData)
+    }
+    func testRSAGenGenEncryptAndDecryptWithDifferentKeysMakesDifferentMessage() {
+        let testString: String = "Hello World!"
+        let testData: Data = testString.data(using: .utf8)!
+        
+        let rsaPrivateKey1: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+        let rsaPrivateKey2: RSAKeyPair = SimpleSwiftCrypto.generateRandomRSAKeyPair()!
+        let encryptedData1: Data = rsaPrivateKey1.extractPublicKey().encrypt(data: testData)!
+        let encryptedData2: Data = rsaPrivateKey2.extractPublicKey().encrypt(data: testData)!
+        let decryptedData1: Data = rsaPrivateKey1.decrypt(data: encryptedData1)!
+        let decryptedData2: Data = rsaPrivateKey2.decrypt(data: encryptedData2)!
+        
+        XCTAssertEqual(testData, testData)
+        XCTAssertNotEqual(testData, encryptedData1)
+        XCTAssertNotEqual(testData, encryptedData2)
+        XCTAssertNotEqual(encryptedData1, encryptedData2)
+        XCTAssertNotEqual(testData, encryptedData1)
+        XCTAssertNotEqual(testData, encryptedData2)
+        XCTAssertNotEqual(encryptedData1, decryptedData1)
+        XCTAssertNotEqual(encryptedData2, decryptedData2)
+        XCTAssertEqual(testData, decryptedData1)
+        XCTAssertEqual(testData, decryptedData2)
+    }
+    
+    func testRSAExportAndLoads() {
+        let rsaPublicKey: RSAPublicKey = SimpleSwiftCrypto.generateRandomRSAKeyPair()!.extractPublicKey()
+        let rsaPublicKeyAsData: Data = rsaPublicKey.export()!
+        let sameRsaPublicKeyFromData: RSAPublicKey = .load(rsaPublicKeyData: rsaPublicKeyAsData)!
+        
+        // We should have different instances of SecKey even though they should be the same public key
+        XCTAssertNotEqual(rsaPublicKey.__debug_publicKey, sameRsaPublicKeyFromData.__debug_publicKey)
+        
+        // To test that they have the same public key, we will see if they produce the same encypted data
+        let testString: String = "Hello World!"
+        let testData: Data = testString.data(using: .utf8)!
+        let encryptedData1: Data = rsaPublicKey.encrypt(data: testData)!
+        let encryptedData2: Data = sameRsaPublicKeyFromData.encrypt(data: testData)!
+        XCTAssertNotEqual(encryptedData1, encryptedData2)
+    }
+}
